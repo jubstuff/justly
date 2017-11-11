a8da0c411c4a555605c0717bb4690349be2e33be
Giustino Borzacchiello <giustinob@gmail.com>
2017-11-11 23:37:14 +0100

    Add PHPUnit and fake test

---
+++ tests/UrlEntityTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Justly;
+
+use PHPUnit\Framework\TestCase;
+
+class UrlEntityTest extends TestCase
+{
+    public function testFalse()
+    {
+        $this->assertFalse(true);
+
+    }
+
+}
